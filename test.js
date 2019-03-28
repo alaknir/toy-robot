ca593752d73ecfe2ef3e01d6cacfdaa89a4fe969
Niranjan kumar <alaknir@gmail.com>
2019-03-28 14:42:25 +0530

    noting tests

---
+++ test.js
@@ -0,0 +1,8 @@
+// create a layout
+// create a command palett(text box)
+// create button for place, move, report, rotate left, rotate right
+// create a event for document key press
+// set default values
+// function proceed
+// function getPossibleDirections
+// function validateStep
